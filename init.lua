aa92262f42eba5d0c810dc03aaae8bac0248872c
kun <kun.chen@qizhidao.com>
2022-05-31 17:03:11 +0800

    Add: force rime input method in all application

--- init.lua
+++ init.lua
@@ -11,26 +11,32 @@
 --                            ** Requires **                            --
 -- -----------------------------------------------------------------------
 -- require "key-binding"
-require "network-binding"
+-- require("network-binding")
 
-require "toggle-application"
-require "windows"
+require("toggle-application")
+-- require("windows")
+require("input-method")
 
 -- -----------------------------------------------------------------------
 --                            ** For Debug **                           --
 -- -----------------------------------------------------------------------
-function reloadConfig(files)
-    local doReload = false
-    for _, file in pairs(files) do
-        if file:sub(-4) == ".lua" then doReload = true end
-    end
-    if doReload then
-        hs.reload()
-        hs.alert.show('Config Reloaded')
-    end
+function reloadConfig(paths, flagTables)
+	local doReload = false
+	for _, path in pairs(paths) do
+		print("reloadConfig path:" .. path)
+		if string.find(path, ".lua") then
+			print("has lua file")
+			doReload = true
+			break
+		end
+	end
+	if doReload then
+		hs.reload()
+	end
 end
 hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
 
--- Well, sometimes auto-reload is not working, you know u.u
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "n", function() hs.reload() end)
+hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "n", function()
+	hs.reload()
+end)
 hs.alert.show("Config loaded")
---
+++ input-method.lua
@@ -0,0 +1,25 @@
+-- helper hotkey to figure out the app path and name of current focused window
+hs.hotkey.bind({ "ctrl", "cmd" }, ".", function()
+	hs.alert.show(
+		"App path:        "
+			.. hs.window.focusedWindow():application():path()
+			.. "\n"
+			.. "App name:      "
+			.. hs.window.focusedWindow():application():name()
+			.. "\n"
+			.. "BundleID:    "
+			.. hs.window.focusedWindow():application():bundleID()
+			.. "\n"
+			.. "IM source id:  "
+			.. hs.keycodes.currentSourceID()
+	)
+end)
+
+function applicationWatcher(appName, eventType, appObject)
+	if eventType == hs.application.watcher.activated or eventType == hs.application.watcher.launched then
+		hs.keycodes.currentSourceID("im.rime.inputmethod.Squirrel.Rime")
+	end
+end
+
+appWathcer = hs.application.watcher.new(applicationWatcher)
+appWathcer:start()
